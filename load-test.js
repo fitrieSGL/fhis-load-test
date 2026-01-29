d9900f1ba0f6435d748a0c9859bb86fc2ee3b7b5
FitrieSGL <fitrie@siagalabs.com>
2026-01-29 10:53:04 +0800

    feat: update code

--- load-test.js
+++ load-test.js
@@ -3,13 +3,14 @@ import { check, sleep } from 'k6';
 
 // Test configuration
 export const options = {
-  // stages: [
-  //   { duration: '30s', target: 10 },  // Ramp up to 10 users over 30 seconds
-  //   { duration: '1m', target: 10 },   // Stay at 10 users for 1 minute
-  //   { duration: '30s', target: 0 },   // Ramp down to 0 users over 30 seconds
-  // ],
+  // Realistic approach
   stages: [
-    { duration: '5s', target: 10 },
+    { duration: '30s', target: 10 },   // Warm up with 10 users
+    { duration: '1m', target: 50 },    // Increase to 50
+    { duration: '1m', target: 100 },   // Increase to 100
+    { duration: '1m', target: 250 },   // Increase to 250
+    { duration: '1m', target: 500 },   // Peak at 500
+    { duration: '30s', target: 0 },    // Ramp down
   ],
   thresholds: {
     http_req_duration: ['p(95)<500'], // 95% of requests should be below 500ms
@@ -69,13 +70,38 @@ function testMap() {
 }
 
 
+function testSearchFh() {
+  const BASE_URL = "http://localhost:3002/fhis-api/v1";
+  const URL = `${BASE_URL}/firehydrant/search/filter?take=10&offset=0`;
+  const BEARER_TOKEN = 'BEARER_TOKEN';
+  const params = {
+    headers: {
+      'Authorization': `Bearer ${BEARER_TOKEN}`,
+      'Content-Type': 'application/json',
+    },
+  };
+  const response = http.get(URL, params);
+
+  // Validate the response
+  check(response, {
+    'status is 200': (r) => r.status === 200,
+    'response time < 500ms': (r) => r.timings.duration < 500,
+    // 'body contains text': (r) => r.body.includes('Collection of simple web-pages'),
+  });
+
+  // Simulate user think time
+  sleep(1);
+}
+
+
 
 
 
 // Main test function
 export default function () {
   // testDashboard();
-  testMap();
+  // testMap();
+  testSearchFh();
 }
 
 // Setup function (runs once before the test starts)
